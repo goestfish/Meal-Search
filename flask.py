1ec3d0f7d945221d8e989ca2a519ea84c7c7a03f
goestfish <114896816+goestfish@users.noreply.github.com>
2024-05-21 22:04:48 -0400

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,46 @@
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+    if not query:
+        return jsonify([])
+
+    res = es.search(index="food_index", body={
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["name", "recipe", "description"]
+            }
+        }
+    })
+    return jsonify(res['hits']['hits'])
+
+def create_index():
+    es.indices.create(index='food_index', body={
+        "mappings": {
+            "properties": {
+                "name": { "type": "text" },
+                "recipe": { "type": "text" },
+                "video": { "type": "text" },
+                "description": { "type": "text" }
+            }
+        }
+    }, ignore=400)
+
+def index_data():
+    food_data = [
+        {
+            "name": "Pasta",
+            "recipe": "Boil water, add pasta, cook for 10 minutes.",
+            "video": "http://example.com/video",
+            "description": "A classic Italian dish."
+        },
+        {
+            "name": "Pizza",
+            "recipe": "Preheat oven, add toppings, bake for 15 minutes.",
+            "video": "http://example.com/video2",
+            "description": "A popular Italian dish."
+        }
+    ]
+
+    for food in food_data:
+        es.index(index="food_index", body=food)
